ddfea7ca69134abc670e48c301049993995ed681
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2024-02-11 20:08:04 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+// class hello{
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!"); 
+//     }
+// }
+
+class Public {
+    public static void main (String [] args ) {
+        System.out.println("I have a car ");
+    }
+}
